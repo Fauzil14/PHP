1a8a6f0d453d8cddf6a9da87c34fe5294ec907e9
Fauzil14 <fauzilazim39@gmail.com>
2020-12-12 15:30:29 +0700

    add costum denominations as per request

--- modulus.php
+++ modulus.php
@@ -17,27 +17,71 @@ function comparison($amount) {
 }
 
 // fungsi untuk mencari pecahan kembalian
-function changePiece($change) {
+function changePiece($change, $request = null) {
     $i = 0;
     $mc = [];
     do {
-        $comparison = comparison($change);
-        
-        $mod = $change%$comparison;
-        $times = ($change-$mod)/$comparison;
-        if($times > 1) {
-            $mc[] = "Rp " . number_format($comparison, '2', ',' , '.') . " X " . $times; 
-        } else {
-            $mc[] = "Rp " . number_format($comparison, '2', ',', '.');
+        // Note : swtich statement will not run if the value of the expression is null
+        switch (TRUE) {
+            case is_null($request):
+                $comparison = comparison($change);
+                break;
+            case is_integer($request): 
+                $comparison = $request;
+                $request = null; // nullified the request variable so that the case didn't run after one iteration
+                break;
+            case is_array($request): 
+                $comparisons = $request;
+                break;
         }
-        $change = $change%$comparison; 
+       
+        if(!is_array($request)) {
+            $mod = $change%$comparison;
+            $times = ($change-$mod)/$comparison;
+            if($times > 1) {
+                $mc[] = [ 
+                            'nomminal' => $comparison, 
+                            'times'    => $times 
+                        ]; 
+            } else {
+                $mc[] = [ 
+                            'nomminal' => $comparison, 
+                            'times'    => $times 
+                        ];
+            }
+            $change = $change%$comparison;
+        } else {
+            $request = null; // nullified the request variable so that the case didn't run after one iteration
+            foreach ($comparisons as $comparison) {
+                $mc[] = [ 
+                            'nominal' => $comparison['nominal'],
+                            'times' => $comparison['times']
+                        ];
+                // get the total request as per nominal to array;
+                $substraction[] = $comparison['nominal'] * $comparison['times'];
+            }
+            // sum up total request to a single nominal
+            $substraction = array_sum($substraction);
+            $change = $change - $substraction;
+        }
+
+        // return [    
+        //             'comparison'   => $comparison, 
+        //             'change'       => $change, 
+        //             'mc'           => $mc, 
+        //             'substraction' => $substraction,
+        //             'request' => $request
+        //         ];
         $i++;
     } while($change > 0);
     
     return $mc;
 }
 
-var_dump(changePiece(298700));
-// print_r(comparison(99500));
+print_r(changePiece(306700, [['nominal' => 100000, 'times' => 2], ['nominal' => 50000, 'times' => 2], ['nominal' => 200, 'times' => 8]]));
+echo "\n";
+print_r(changePiece(306700, 20000));
+echo "\n";
+print_r(changePiece(306700));
 
 ?>
