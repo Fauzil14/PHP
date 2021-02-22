4d0549d41733d7a8b6fc7b2a19e4a4c616a5a5b8
Fauzil14 <fauzilazim39@gmail.com>
2021-02-22 21:48:07 +0700

    deretan prima v1 v2

--- modulus.php
+++ modulus.php
@@ -7,11 +7,13 @@
 $denominations = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100];
 
 // fungsi untuk memfilter pecahan berdasarkan request tanpa ketentuan jumlah(times)
-function denomfil($filter = null) {
+function denomfil($filter) {
     global $denominations;
+    
     $top = array_filter($denominations, function($value) use ($filter) {
         return in_array($value, $filter);
     });
+
     $bottom = array_filter($denominations, function($value) use ($top) {
         if($value < min($top)) {
             return $value;
@@ -37,14 +39,14 @@ function comparison($amount) {
 
 // fungsi untuk mencari pecahan kembalian
 function changePiece($change, $request = null) {
-    $i = 0;
     $mc = [];
     do {
         // jika request tidak memiliki jumlah(times) maka filter denominations
-        if(is_array($request) && !array_key_exists('times', $request)) {
-            denomfil($request);
-            $request = null;
-        }
+        // if(is_array($request) && !isset($request['times'])) {
+        //     // var_dump($request); die;
+        //     denomfil($request);
+        //     $request = null;
+        // }
 
         // Note : swtich statement will not run if the value of the expression is null
         switch (TRUE) {
@@ -56,7 +58,14 @@ function changePiece($change, $request = null) {
                 $request = null; // nullified the request variable so that the case didn't run after one iteration
                 break;
             case is_array($request): 
-                $comparisons = $request;
+                if(isset($request['times'])) {
+                    $comparisons = $request;
+                    echo "it has to be here";
+                } else {
+                    
+                    denomfil($request);
+                    $request = null;
+                }
                 break;
         }
        
@@ -84,7 +93,6 @@ function changePiece($change, $request = null) {
             $change = $change - $substraction;
         }
 
-        $i++;
     } while($change > 0);
     
     
@@ -93,11 +101,11 @@ function changePiece($change, $request = null) {
     return $mc;
 }
 
-// print_r(changePiece(306700, [['nominal' => 100000, 'times' => 2], ['nominal' => 50000, 'times' => 2], ['nominal' => 200, 'times' => 7]]));
-// echo "\n";
+print_r(changePiece(306700, [['nominal' => 100000, 'times' => 2], ['nominal' => 50000, 'times' => 2], ['nominal' => 200, 'times' => 7]]));
+echo "\n";
 // print_r(changePiece(306700, 20000));
 // echo "\n";
-print_r(changePiece(386700, [50000, 5000, 2000])); // bug here => the result didn't return 20000 because 300000%50000 = 0
+// print_r(changePiece(386700, [50000, 5000, 2000])); // bug here => the result didn't include 20000 because 300000%50000 = 0
 // echo "\n";
 // print_r(changePiece(306700));
 
--- primanumber.php
+++ primanumber.php
@@ -12,11 +12,26 @@ function kelipatan($number) {
     return $kelipatan;
 }
 
-for($i = 2; $i <= $n; $i++) {
-    if( count(kelipatan($i)) == 2) {
-        $prima[] = $i;
+function prima_v1() {
+    global $n;
+    for($i = 2; $i <= $n; $i++) {
+        if( count(kelipatan($i)) == 2 ) {
+            $prima[] = $i;
+        }
     }
+    return $prima;
 }
 
-print_r($prima);
+function prima_v2() {
+    global $n;
+    for($i = 2; $i <= $n; $i++) {
+        if( kelipatan($i)[0] == 1 && kelipatan($i)[1] == $i ) {
+            $prima[] = $i;
+        }
+    }
+    return $prima;
+}
+
+print_r(prima_v1()); echo "\n";
+print_r(prima_v2());
 ?>
